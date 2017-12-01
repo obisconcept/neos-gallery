650c230c3e9ee844d6c47c338d2e275fccb438df
Max Schmidt <maximilianschmidt404@gmail.com>
2017-12-01 11:51:54 +0100

    Extended slide transition duration

--- Resources/Private/Assets/JavaScript/main.js
+++ Resources/Private/Assets/JavaScript/main.js
@@ -1,3 +1,8 @@
 $(function() {
-  if($('.zoombox').length && !window.surpressGalleryInit && !$('#neos-application').length) $('.zoombox').zoombox();
+  if($('.zoombox').length && !window.surpressGalleryInit && !$('#neos-application').length) {
+    $('.zoombox').zoombox({
+      slideTransitionSpeed: 500,
+      slideTransition: 'ease-in-out'
+    });
+  }
 })
--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -3245,5 +3245,10 @@
 }( jQuery ));
 
 $(function() {
-  if($('.zoombox').length && !window.surpressGalleryInit && !$('#neos-application').length) $('.zoombox').zoombox();
+  if($('.zoombox').length && !window.surpressGalleryInit && !$('#neos-application').length) {
+    $('.zoombox').zoombox({
+      slideTransitionSpeed: 500,
+      slideTransition: 'ease-in-out'
+    });
+  }
 })
